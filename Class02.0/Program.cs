63cbb15b1f6f8761080c2e60f756944bc04be83f
hodaka-endo <porize1olive427@yahoo.co.jp>
2020-07-14 17:09:20 +0900

    ちょっと変更

--- Class02.0/Program.cs
+++ Class02.0/Program.cs
@@ -6,7 +6,7 @@ namespace Class02._0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("コミット");
         }
     }
 }
